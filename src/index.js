bc5ed4f05db805f6f72c3c3f731dbcb555657703
Reinny <reynsth@gmail.com>
2021-02-12 11:05:05 -0400

    :sparkles: Showing whose turn is

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import Board from "./components/Board";
 import Scoreboard from "./components/Scoreboard";
+import CurrentTurn from "./components/CurrentTurn";
 import Options from "./components/Options";
 
 const instanceOfBoard = new Board();
 const instanceOfScoreboard = new Scoreboard();
+const instanceOfCurrentTurn = new CurrentTurn();
 const instanceOfOptions = new Options();
 
 instanceOfBoard.render();
 instanceOfScoreboard.render();
+instanceOfCurrentTurn.render();
 instanceOfOptions.render();
