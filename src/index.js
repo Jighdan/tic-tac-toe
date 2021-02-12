86097cbd29fa623187c59720a6acc03703f108a3
Reinny <reynsth@gmail.com>
2021-02-12 09:02:06 -0400

    :sparkles: Tracking score

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Board from "./components/Board";
+import Scoreboard from "./components/Scoreboard";
 
 const instanceOfBoard = new Board();
+const instanceOfScoreboard = new Scoreboard();
 
 instanceOfBoard.render();
+instanceOfScoreboard.render();
