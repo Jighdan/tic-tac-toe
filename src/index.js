72671c613b041d0fd3c79447a0e1c454cf1ff9a5
Reinny <reynsth@gmail.com>
2021-02-12 10:58:34 -0400

    :sparkles: Added buttons to clear score & board

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import Board from "./components/Board";
 import Scoreboard from "./components/Scoreboard";
+import Options from "./components/Options";
 
 const instanceOfBoard = new Board();
 const instanceOfScoreboard = new Scoreboard();
+const instanceOfOptions = new Options();
 
 instanceOfBoard.render();
 instanceOfScoreboard.render();
+instanceOfOptions.render();
